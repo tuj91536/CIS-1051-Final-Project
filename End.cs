05d74e8bf1bd42f9c5cd416dda5761600fb02677
tuj91536 <74029318+tuj91536@users.noreply.github.com>
2020-12-07 19:31:45 -0500

    Add files via upload

---
+++ End.cs
@@ -0,0 +1,24 @@
+﻿// https://answers.unity.com/questions/161858/startstop-playmode-from-editor-script.html
+// https://youtu.be/lrmNnwhOjgM
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AppHelper
+{
+    #if UNITY_WEBPLAYER
+        public static string webplayerQuitURL = "http://google.com";
+    #endif
+    public static void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL(webplayerQuitURL);
+        #else
+            Application.Quit();
+        #endif
+    }
+}
---
+++ Wait.cs
@@ -0,0 +1,22 @@
+﻿// https://answers.unity.com/questions/161858/startstop-playmode-from-editor-script.html
+// https://youtu.be/lrmNnwhOjgM
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait : MonoBehaviour
+{
+    void Start()
+    {
+        //Start the coroutine we define below named ExampleCoroutine.
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(4);
+        AppHelper.Quit();
+    }
+}
